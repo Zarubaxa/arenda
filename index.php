b1753a34197c70f67dc89c9852d95898d68bf7ed
zarubaxa <zarubaxa@gmail.com>
2024-09-10 00:23:44 +0300

    загрузка на git

---
+++ index.php
@@ -0,0 +1,123 @@
+<?php
+//get_header();
+include 'header.php';
+?>
+<body>
+    <div class="content">
+        <div class="container">
+            <div class="breadcrumbs">
+                <ul>
+                    <li>
+                        <a href="#">Главная</a>
+                    </li>
+                    >
+                    <li>
+                        <a href="#">Аренда</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="top">
+            <div class="container">
+                <div class="img-block">
+                    <a href="#">
+                        <img src="img/top.png" alt="">
+                    </a>
+                </div>
+                <div class="info">
+                    <h1>Аренда в ТУЦ “Сарафан”</h1>
+                    <p class="desc">Торговый центр «Сарафан», расположенный по адресу: г. Тула, ул. Путейская, д.5 (Площадь Московского вокзала), предлагает помещения в аренду от собственника от 25 до 1000 кв. м.</p>
+                    <div class="presentation">
+                        <a href="#">Скачать презентацию <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32" fill="none">
+                                <path d="M16 21.5666L9.56671 15.1333L11 13.7L15 17.7V5.33331H17V17.7L21 13.7L22.4334 15.1333L16 21.5666ZM7.33337 26.6666C6.80004 26.6666 6.33337 26.4666 5.93337 26.0666C5.53337 25.6666 5.33337 25.2 5.33337 24.6666V19.9H7.33337V24.6666H24.6667V19.9H26.6667V24.6666C26.6667 25.2 26.4667 25.6666 26.0667 26.0666C25.6667 26.4666 25.2 26.6666 24.6667 26.6666H7.33337Z" fill="#6B7073"/>
+                            </svg>
+                        </a>
+                    </div>
+                    <div class="advantages">
+                        <p class="title">Преимущества аренды в нашем торговом центре:</p>
+                        <ul>
+                            <li>ТЦ «Сарафан» находится в 5 минутах езды от центра города на площади Московского вокзала на одной из самых оживленных улиц города – Красноармейский проспект.</li>
+                            <li>Рядом жилой массив, пересечение основных транспортных и пешеходных потоков –конечная остановка общественного транспорта, железнодорожный вокзал.</li>
+                            <li>На территории ТЦ «Сарафан» есть бесплатная парковка для клиентов, более 500 машиномест, удобный подъезд.</li>
+                            <li>Ежедневно наш ТЦ посещает порядка 10 000 тысяч человек различной целевой аудитории</li>
+                            <li>Демократичные цены за аренду площади</li>
+                            <li>У нас Вы можете арендовать площадь под магазины белья, одежды и обуви, салоны, точки общепита и др. </li>
+                        </ul>
+                        <p class="notice">Открытие торговой точки в одном из оживленных мест города обеспечит доходность, развитие и процветание Вашего бизнеса.</p>
+                    </div>
+                </div>
+                <div class="shops">
+                    <div class="slider-block">
+                        <div class="slider"></div>
+                        <div class="btn-block">
+
+                        </div>
+                    </div>
+                    <div class="info">
+                        <ul>
+                            <li>Якорные арендаторы - Лента, Детский Мир, Технопарк, Модис, Familia, Читай город, Тутанхамон, Л'Этуаль</li>
+                            <li>Бренды - EWA, Artigli, Gerzedo, Goergo, Du monde, Petek, Neri Karra, Ledy Collection, Paolo Conte, P'Cont, VINZER, Ace, Tonnelli, Milavitsa, и др.</li>
+                            <li>Магазины спортивной одежды и инвентаря: Адреналин, Спорт Лайн</li>
+                            <li>Зона фудкорта - Burger King, Империя вкуса, Про Кофий, Добрая сдоба</li>
+                            <li>Салоны связи: Билайн, Мегафон, YOTA, Связной, Теле2</li>
+                            <li>Зоосад, аптека «Здесь аптека», «Арт-оптика», Fresh Оптика, ковры, салоны штор, текстиль Missis Hatson и Home TEX, салон Арт-Самовар, посуда, подарки, картины, сувениры и другие.</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="char">
+                    <div class="data">
+                        <div class="block">
+                            <p class="title">Общая площадь<br> торгового центра</p>
+                            <p class="desc">25 816 м2</p>
+                        </div>
+                        <div class="block">
+                            <p class="title">Площадь, сдаваемая<br>в аренду</p>
+                            <p class="desc">16 675 м2</p>
+                        </div>
+                        <div class="block">
+                            <p class="title">Парковка<br>(машиномест)
+                            </p>
+                            <p class="desc">500</p>
+                        </div>
+                        <div class="block">
+                            <p class="title">Посещаемость<br>(в неделю)</p>
+                            <p class="desc">100 000 чел</p>
+                        </div>
+                    </div>
+                    <div class="gal">
+                        <ul>
+                            <li>
+                                <a href="#">
+                                    <img src="img/pic1.jpg" alt="">
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <img src="img/pic2.jpg" alt="">
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <img src="img/pic3.jpg" alt="">
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <img src="img/pic4.jpg" alt="">
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <img src="img/pic5.jpg" alt="">
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+<?php
+//get_footer();
+include 'footer.php';
+?>
