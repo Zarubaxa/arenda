b3098908edd929458259d730b4e6d1236b00d2b2
Anar <zarubaxa@gmail.com>
2024-09-17 16:56:02 +0300

    верстка + адаптация

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 //get_header();
 include 'header.php';
 ?>
-<body>
+    <body>
     <div class="content">
         <div class="container">
             <div class="breadcrumbs">
@@ -10,109 +10,182 @@ include 'header.php';
                     <li>
                         <a href="#">Главная</a>
                     </li>
-                    >
-                    <li>
+                    <li class="current">
                         <a href="#">Аренда</a>
                     </li>
                 </ul>
             </div>
-        </div>
-        <div class="top">
-            <div class="container">
+            <div class="top">
                 <div class="img-block">
                     <a href="#">
-                        <img src="img/top.png" alt="">
+                        <img src="img/top.jpg" alt="">
                     </a>
                 </div>
-                <div class="info">
+                <div class="top-info">
                     <h1>Аренда в ТУЦ “Сарафан”</h1>
-                    <p class="desc">Торговый центр «Сарафан», расположенный по адресу: г. Тула, ул. Путейская, д.5 (Площадь Московского вокзала), предлагает помещения в аренду от собственника от 25 до 1000 кв. м.</p>
+                    <p class="desc">Торговый центр «Сарафан», расположенный по адресу: г. Тула, ул. Путейская, д.5
+                        (Площадь Московского вокзала), предлагает помещения в аренду от собственника от 25 до 1000 кв. м.</p>
                     <div class="presentation">
-                        <a href="#">Скачать презентацию <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32" fill="none">
-                                <path d="M16 21.5666L9.56671 15.1333L11 13.7L15 17.7V5.33331H17V17.7L21 13.7L22.4334 15.1333L16 21.5666ZM7.33337 26.6666C6.80004 26.6666 6.33337 26.4666 5.93337 26.0666C5.53337 25.6666 5.33337 25.2 5.33337 24.6666V19.9H7.33337V24.6666H24.6667V19.9H26.6667V24.6666C26.6667 25.2 26.4667 25.6666 26.0667 26.0666C25.6667 26.4666 25.2 26.6666 24.6667 26.6666H7.33337Z" fill="#6B7073"/>
+                        <a href="#">Скачать презентацию
+                            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32" fill="none">
+                                <path d="M16.0007 21.5667L9.56732 15.1333L11.0007 13.7L15.0007 17.7V5.33334H17.0007V17.7L21.0007 13.7L22.434 15.1333L16.0007 21.5667ZM7.33398 26.6667C6.80065 26.6667 6.33398 26.4667 5.93398 26.0667C5.53398 25.6667 5.33398 25.2 5.33398 24.6667V19.9H7.33398V24.6667H24.6673V19.9H26.6673V24.6667C26.6673 25.2 26.4673 25.6667 26.0673 26.0667C25.6673 26.4667 25.2007 26.6667 24.6673 26.6667H7.33398Z" fill="#151516"/>
                             </svg>
                         </a>
                     </div>
                     <div class="advantages">
                         <p class="title">Преимущества аренды в нашем торговом центре:</p>
                         <ul>
-                            <li>ТЦ «Сарафан» находится в 5 минутах езды от центра города на площади Московского вокзала на одной из самых оживленных улиц города – Красноармейский проспект.</li>
-                            <li>Рядом жилой массив, пересечение основных транспортных и пешеходных потоков –конечная остановка общественного транспорта, железнодорожный вокзал.</li>
-                            <li>На территории ТЦ «Сарафан» есть бесплатная парковка для клиентов, более 500 машиномест, удобный подъезд.</li>
+                            <li>ТЦ «Сарафан» находится в 5 минутах езды от центра города на площади Московского вокзала
+                                на одной из самых оживленных улиц города – Красноармейский проспект.
+                            </li>
+                            <li>Рядом жилой массив, пересечение основных транспортных и пешеходных потоков –конечная
+                                остановка общественного транспорта, железнодорожный вокзал.
+                            </li>
+                            <li>На территории ТЦ «Сарафан» есть бесплатная парковка для клиентов, более 500 машиномест,
+                                удобный подъезд.
+                            </li>
                             <li>Ежедневно наш ТЦ посещает порядка 10 000 тысяч человек различной целевой аудитории</li>
                             <li>Демократичные цены за аренду площади</li>
-                            <li>У нас Вы можете арендовать площадь под магазины белья, одежды и обуви, салоны, точки общепита и др. </li>
+                            <li>У нас Вы можете арендовать площадь под магазины белья, одежды и обуви, салоны, точки
+                                общепита и др.
+                            </li>
                         </ul>
-                        <p class="notice">Открытие торговой точки в одном из оживленных мест города обеспечит доходность, развитие и процветание Вашего бизнеса.</p>
+                        <p class="notice">Открытие торговой точки в одном из оживленных мест города обеспечит
+                            доходность, развитие и процветание Вашего бизнеса.</p>
                     </div>
                 </div>
-                <div class="shops">
+            </div>
+            <div class="shops">
+                <h2>Более 130 бутиков и магазинов уже арендуют у нас помещения</h2>
+                <div class="shops-content">
                     <div class="slider-block">
-                        <div class="slider"></div>
-                        <div class="btn-block">
-
+                        <div class="owl-carousel">
+                            <div class="slide">
+                                <a href="img/slide1.jpg">
+                                    <img src="img/slide1.jpg" alt="">
+                                </a>
+                            </div>
+                            <div class="slide">
+                                <a href="img/slide2.jpg">
+                                    <img src="img/slide2.jpg" alt="">
+                                </a>
+                            </div>
+                            <div class="slide">
+                                <a href="img/slide3.jpg">
+                                    <img src="img/slide3.jpg" alt="">
+                                </a>
+                            </div>
+                            <div class="slide">
+                                <a href="img/slide1.jpg">
+                                    <img src="img/slide1.jpg" alt="">
+                                </a>
+                            </div>
+                            <div class="slide">
+                                <a href="img/slide2.jpg">
+                                    <img src="img/slide2.jpg" alt="">
+                                </a>
+                            </div>
+                            <div class="slide">
+                                <a href="img/slide3.jpg">
+                                    <img src="img/slide3.jpg" alt="">
+                                </a>
+                            </div>
+                            <div class="slide">
+                                <a href="img/slide1.jpg">
+                                    <img src="img/slide1.jpg" alt="">
+                                </a>
+                            </div>
+                            <div class="slide">
+                                <a href="img/slide2.jpg">
+                                    <img src="img/slide2.jpg" alt="">
+                                </a>
+                            </div>
+                            <div class="slide">
+                                <a href="img/slide3.jpg">
+                                    <img src="img/slide3.jpg" alt="">
+                                </a>
+                            </div>
+                        </div>
+                        <div class="navigation-owl">
+                            <button class="prev">
+                                <img src="img/left.png" alt="">
+                            </button>
+                            <div class="navigation-dots">
+                                <button role="button" class="owl-dot active"><span></span></button>
+                                <button role="button" class="owl-dot"><span></span></button>
+                                <button role="button" class="owl-dot"><span></span></button>
+                            </div>
+                            <button class="next">
+                                <img src="img/right.png" alt="">
+                            </button>
                         </div>
                     </div>
                     <div class="info">
                         <ul>
-                            <li>Якорные арендаторы - Лента, Детский Мир, Технопарк, Модис, Familia, Читай город, Тутанхамон, Л'Этуаль</li>
-                            <li>Бренды - EWA, Artigli, Gerzedo, Goergo, Du monde, Petek, Neri Karra, Ledy Collection, Paolo Conte, P'Cont, VINZER, Ace, Tonnelli, Milavitsa, и др.</li>
+                            <li>Якорные арендаторы - Лента, Детский Мир, Технопарк, Модис, Familia, Читай город, Тутанхамон,
+                                Л'Этуаль
+                            </li>
+                            <li>Бренды - EWA, Artigli, Gerzedo, Goergo, Du monde, Petek, Neri Karra, Ledy Collection, Paolo
+                                Conte, P'Cont, VINZER, Ace, Tonnelli, Milavitsa, и др.
+                            </li>
                             <li>Магазины спортивной одежды и инвентаря: Адреналин, Спорт Лайн</li>
                             <li>Зона фудкорта - Burger King, Империя вкуса, Про Кофий, Добрая сдоба</li>
                             <li>Салоны связи: Билайн, Мегафон, YOTA, Связной, Теле2</li>
-                            <li>Зоосад, аптека «Здесь аптека», «Арт-оптика», Fresh Оптика, ковры, салоны штор, текстиль Missis Hatson и Home TEX, салон Арт-Самовар, посуда, подарки, картины, сувениры и другие.</li>
+                            <li>Зоосад, аптека «Здесь аптека», «Арт-оптика», Fresh Оптика, ковры, салоны штор, текстиль
+                                Missis Hatson и Home TEX, салон Арт-Самовар, посуда, подарки, картины, сувениры и другие.
+                            </li>
                         </ul>
                     </div>
                 </div>
-                <div class="char">
-                    <div class="data">
-                        <div class="block">
-                            <p class="title">Общая площадь<br> торгового центра</p>
-                            <p class="desc">25 816 м2</p>
-                        </div>
-                        <div class="block">
-                            <p class="title">Площадь, сдаваемая<br>в аренду</p>
-                            <p class="desc">16 675 м2</p>
-                        </div>
-                        <div class="block">
-                            <p class="title">Парковка<br>(машиномест)
-                            </p>
-                            <p class="desc">500</p>
-                        </div>
-                        <div class="block">
-                            <p class="title">Посещаемость<br>(в неделю)</p>
-                            <p class="desc">100 000 чел</p>
-                        </div>
-                    </div>
-                    <div class="gal">
-                        <ul>
-                            <li>
-                                <a href="#">
-                                    <img src="img/pic1.jpg" alt="">
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#">
-                                    <img src="img/pic2.jpg" alt="">
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#">
-                                    <img src="img/pic3.jpg" alt="">
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#">
-                                    <img src="img/pic4.jpg" alt="">
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#">
-                                    <img src="img/pic5.jpg" alt="">
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
+            </div>
+            <div class="char">
+                <div class="gal">
+                    <ul>
+                        <li class="data">
+                            <div class="block">
+                                <p class="title">Общая площадь<br> торгового центра</p>
+                                <p class="desc">25 816 м2</p>
+                            </div>
+                            <div class="block">
+                                <p class="title">Площадь, сдаваемая<br>в аренду</p>
+                                <p class="desc">16 675 м2</p>
+                            </div>
+                            <div class="block">
+                                <p class="title">Парковка<br>(машиномест)
+                                </p>
+                                <p class="desc">500</p>
+                            </div>
+                            <div class="block">
+                                <p class="title">Посещаемость<br>(в неделю)</p>
+                                <p class="desc">100 000 чел</p>
+                            </div>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <img src="img/pic1.jpg" alt="">
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <img src="img/pic2.jpg" alt="">
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <img src="img/pic3.jpg" alt="">
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <img src="img/pic4.jpg" alt="">
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <img src="img/pic5.jpg" alt="">
+                            </a>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </div>
--- js/main.js
+++ js/main.js
@@ -1,1 +1,60 @@
-console.log(999);
+$(document).ready(function () {
+    console.log("ready!");
+    let owl = $('.owl-carousel');
+    owl.owlCarousel({
+        margin: 46,
+        nav: false,
+        responsiveClass: true,
+        loop: true,
+        dotsContainer: '.navigation-dots',
+        dots: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 2
+            },
+            1000: {
+                items: 3,
+                margin: 75
+            },
+            1640: {
+                items: 3,
+                margin: 75
+            },
+            1920: {
+                items: 3,
+                margin: 46
+            }
+        }
+    });
+
+    $('.navigation-owl .next').click(function () {
+        owl.trigger('next.owl.carousel');
+    });
+    $('.navigation-owl .prev').click(function () {
+        owl.trigger('prev.owl.carousel');
+    });
+
+    $('header .block .nav .mobile-btn button').click(function () {
+        let mobileBtn = $('header .block .nav .mobile-btn button');
+        if (mobileBtn.hasClass('active')) {
+            mobileBtn.removeClass('active');
+        } else mobileBtn.addClass('active');
+
+        if ($(window).width() > 767) {
+            $('header .big-menu').toggleClass('active');
+        }
+        if ($(window).width() < 768) {
+            let mobileBlock = $('header .mobile-block');
+            if(mobileBlock.hasClass('active')) {
+                mobileBlock.removeClass('active');
+            } else {
+                mobileBlock.addClass('active');
+            }
+        }
+    });
+
+
+});
